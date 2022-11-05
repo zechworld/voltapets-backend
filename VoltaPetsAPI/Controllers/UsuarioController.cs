97cb6687868dedc5885d53d07e1940e74636b3b0
Epitafio <nicolas.roman09@inacapmail.cl>
2022-11-04 22:09:40 -0300

    WIP:Controlador Usuario

--- VoltaPetsAPI/Controllers/UsuarioController.cs
+++ VoltaPetsAPI/Controllers/UsuarioController.cs
@@ -1,11 +1,52 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using VoltaPetsAPI.Data;
+using VoltaPetsAPI.Helpers;
+using VoltaPetsAPI.Models;
 
 namespace VoltaPetsAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly VoltaPetsContext _context;
+
+        public UsuarioController(VoltaPetsContext context)
+        {
+            _context = context;
+        }
+
+        [Route("Registar")]
+        [HttpPost]
+        public async Task<IActionResult> RegistrarUsuario([FromBody] Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(await _context.Usuarios.Where(u => u.Email == usuario.Email).AnyAsync())
+            {
+                return BadRequest("El Correo ya tiene un usuario asociado");
+            }
+
+            usuario.Password = Encriptacion.GetSHA256(usuario.Password);
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+
+
+
+
+
+
     }
 }
