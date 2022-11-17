ce0927a1c68bf49d31a450fa5be997015f7ef667
Epitafio <nicolas.roman09@inacapmail.cl>
2022-11-16 23:07:45 -0300

    WIP:Se agrego registrar y consultar compromisos y recordatorios

---
+++ VoltaPetsAPI/Controllers/RecordatorioController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using VoltaPetsAPI.Data;
+using VoltaPetsAPI.Models;
+using VoltaPetsAPI.Models.ViewModels;
+
+namespace VoltaPetsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordatorioController : ControllerBase
+    {
+        private readonly VoltaPetsContext _context;
+
+        public RecordatorioController(VoltaPetsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("Registrar")]
+        [Authorize(Policy = "Tutor")]
+        public async Task<IActionResult> RegistrarRecordatorio(RecordatorioVM recordatorioVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            Recordatorio recordatorio = new Recordatorio
+            {
+                CodigoMascota = (int)recordatorioVM.CodigoMascota,
+                Titulo = recordatorioVM.Titulo,
+                Descripcion = recordatorioVM.Descripcion,
+                FechaPublicacion = DateTime.Now
+            };
+
+            _context.Recordatorios.Add(recordatorio);
+            var registroRecordatorio = await _context.SaveChangesAsync();
+
+            if(registroRecordatorio <= 0)
+            {
+                return BadRequest(new { mensaje = "No se pudo registrar el recordatorio" });
+            }
+
+            return Ok(new { mensaje = "Recordatorio registrado con éxito" });
+
+        }
+
+        [HttpGet]
+        [Route("Obtener/{codigoMascota:int}")]
+        [Authorize(Policy = "Tutor")]
+        public async Task<IActionResult> ObtenerRecordatoriosMascota(int codigoMascota)
+        {
+            var recordatoriosMascota = await _context.Recordatorios
+                .Where(rc => rc.CodigoMascota == codigoMascota)
+                .OrderByDescending(rc => rc.FechaPublicacion)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(recordatoriosMascota);
+
+        }
+
+    }
+}
---
+++ VoltaPetsAPI/Models/ViewModels/RecordatorioVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VoltaPetsAPI.Models.ViewModels
+{
+    public class RecordatorioVM
+    {
+        [Required(ErrorMessage = "No se recibio codigo mascota")]
+        public int? CodigoMascota { get; set; }
+
+        [Required(ErrorMessage = "El campo Titulo es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El Titulo debe tener como maximo 100 caracteres")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "El campo Descripcion es obligatorio")]
+        [MaxLength(500, ErrorMessage = "La Descripcion debe tener como maximo 500 caracteres")]
+        public string Descripcion { get; set; }
+
+    }
+}
