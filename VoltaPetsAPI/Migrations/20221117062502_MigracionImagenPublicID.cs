3d10a993d606455d79deef4b7d0dab178f275f20
Epitafio <nicolas.roman09@inacapmail.cl>
2022-11-17 03:30:36 -0300

    WIP: Se creo Migracion para agregar public id en tabla imagen

---
+++ VoltaPetsAPI/Migrations/20221117062502_MigracionImagenPublicID.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VoltaPetsAPI.Migrations
+{
+    public partial class MigracionImagenPublicID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "imagen",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "imagen");
+        }
+    }
+}
